7dffaa36eed009beacfb66f32d2af38897d4eed9
MJTemple <34741764+MJTemple@users.noreply.github.com>
2021-07-02 14:10:36 +0100

    Update config.py
    Replace emline_templates with v20210625

--- config.py
+++ config.py
@@ -9,11 +9,11 @@ Idea is to load emission line templates, host galaxy template, and extinction
 curve once to minimise i/o.
 
 @author: Matthew Temple
-Last edit 2021 March 13
+Last edit 2021 July 02: v20210625 emline_templates and associated model params.
 """
 import numpy as np
 
-f1 = 'qsosed_emlines.dat'
+f1 = 'qsosed_emlines_20210625.dat'
 emline_template = np.genfromtxt(f1, unpack=True)
 # wav, median_emlines, continuum, peaky_line, windy_lines, narrow_lines
 
@@ -26,14 +26,14 @@ f3 = 'pl_ext_comp_03.sph'
 reddening_curve = np.genfromtxt(f3, unpack=True)
 
 # fit to DR16Q median 2sigma-clipped colours in multi-imag bins
-params = dict(plslp1=-0.347,
-              plslp2=0.739,
-              plstep=-1.0,  # (not fit for)
-              plbrk1=3800.,
-              tbb=1187.,
-              plbrk3=1200.,  # (not fit for)
-              bbnorm=4.08,
-              scal_emline=-1.,
+params = dict(plslp1=-0.349,
+              plslp2=0.593,
+              plstep=-1.0,    # (not fit for)
+              plbrk1=3880.,
+              tbb=1243.6,
+              plbrk3=1200,   # (not fit for)
+              bbnorm=3.961,
+              scal_emline=-0.9936,
               emline_type=None,
               scal_halpha=1.,
               scal_lya=1.,
@@ -47,11 +47,11 @@ params = dict(plslp1=-0.347,
                                      -26.6, -27.1, -27.6, -27.9, -28.1, -28.4,
                                      -28.6, -28.9]]),
               M_i=None,
-              beslope=0.189,
-              benorm=-27.,  # (not fit for)
+              beslope=0.183,
+              benorm=-27.,    # (not fit for)
               bcnorm=False,
               lyForest=True,
-              lylim=912.,  # (not fit for)
+              lylim=912,   # (not fit for)
               gflag=True,
-              fragal=0.247,
-              gplind=0.723)
+              fragal=0.244,
+              gplind=0.684)
