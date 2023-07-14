a395972fc4fbfbc767d7ae62a803b001b9929cfe
MJTemple <34741764+MJTemple@users.noreply.github.com>
2023-07-14 11:58:34 +0100

    Update setup.py
    Add package data

--- setup.py
+++ setup.py
@@ -8,9 +8,17 @@ setup(
     author='Matthew Temple',
     author_email='Matthew.Temple@mail.udp.cl',
     license='MIT',
+    package_dir={'qsogen': 'qsogen'},
+    package_data={'qsogen': ['S0_template_norm.sed',
+                             'pl_ext_comp_03.sph',
+                             'qsosed_emlines_20210625.dat',
+                             'vega_2007.lis',
+                             'filters/*.filter',]
+                   },
     packages=['qsogen'],
     install_requires=['numpy',
                       'scipy',
                       'astropy',
                       ],
+    include_package_data=True,
 )
