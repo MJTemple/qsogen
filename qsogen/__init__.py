915e71e0abb098d13dd5463f2b6c29f6760ff8ec
MJTemple <34741764+MJTemple@users.noreply.github.com>
2023-07-15 12:16:31 +0100

    Update __init__.py
    Define __all__

--- qsogen/__init__.py
+++ qsogen/__init__.py
@@ -1,3 +1,4 @@
 # from config import *
-from qsosed import Quasar_sed
-from model_colours import get_mags, get_colours
+# from qsosed import Quasar_sed
+# from model_colours import get_mags, get_colours
+__all__ = ["qsosed.Quasar_sed", "model_colours.get_mags", "model_colours.get_colours"]
